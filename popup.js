40ba5c93971ba72d0e7daf05b81ef04e047fdb48
vincebwe <vince.zhou@outlook.com>
2023-01-23 19:05:57 -0800

    finish adding company to local list function

--- popup.js
+++ popup.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+let current_url = "";
+
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    current_url = tabs[0].url;
+    // use `url` here inside the callback because it's asynchronous!
+});
+
+document.getElementById("add-job").addEventListener("click", async () => {
+    let current_company = document.getElementById("company-name-input").value;
+    console.log(current_company);
+    let result = await chrome.storage.local.get({"company_list": {}});
+
+    console.log(result);
+    result["company_list"][current_company] = current_url;
+    await chrome.storage.local.set({"company_list": result["company_list"]});
+})
+
